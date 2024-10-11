adec356c86e11bc00aa94cccc99824f30beb0144
OtBbNA <tosha.kostylev.97@mail.ru>
2024-10-11 23:24:13 +0300

    Front: - update index.html, header.html and nav.html templates (add new menu item, add media queries), - update menuScript.js for media queries, - update css styles files for pageStyle.css, indexContentStyle.css - update completed.svg - new img - add create.html, one.html and update.html templates, and styles for it - add new img for nav (delete.svg, edit.svg, update.svg, check.svg) - add new font-weight for YsabeauOffice

--- src/main/resources/static/js/menuScript.js
+++ src/main/resources/static/js/menuScript.js
@@ -2,31 +2,52 @@ const menu = document.querySelector('.header__registration__button');
 const nav = document.querySelector('.nav');
 const elements = document.querySelectorAll('.nav__menu__list__name');
 const menuState = sessionStorage.getItem('menuSwitcher');
+var switcher = 1;
+var menuStateBuffer;
 
 if (menuState === 'collapsed') {
-    elements.forEach(element => {
-        nav.style.width = '3.5%';
-        element.style.visibility = 'hidden';
-    });
+    changeStyle('50px', 'hidden');
 } else {
-    elements.forEach(element => {
-        nav.style.width = '20%';
-        element.style.visibility = 'visible';
-    });
+    changeStyle('200px', 'visible');
 }
 
 menu.addEventListener("click", function() {
-    if (nav.style.width === '3.5%') {
-        elements.forEach(element => {
-            nav.style.width = '20%';
-            element.style.visibility = 'visible';
-        });
+    resize()
+});
+
+function resize() {
+    if (nav.style.width === '50px') {
+        changeStyle('200px', 'visible');
         sessionStorage.setItem('menuSwitcher', 'expanded');
     } else {
-        elements.forEach(element => {
-            nav.style.width = '3.5%';
-            element.style.visibility = 'hidden';
-        });
+        changeStyle('50px', 'hidden');
         sessionStorage.setItem('menuSwitcher', 'collapsed');
     }
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 1232 && switcher) {
+        changeStyle('50px', 'hidden');
+        switcher = 0;
+        menuStateBuffer = sessionStorage.getItem('menuSwitcher');
+        sessionStorage.setItem('menuSwitcher', 'collapsed');
+    } else if (window.innerWidth >= 1232 && !switcher) {
+        if (menuStateBuffer == 'expanded') {
+            changeStyle('200px', 'visible');
+            switcher = 1;
+            sessionStorage.setItem('menuSwitcher', 'expanded');
+        } else {
+            changeStyle('50px', 'hidden');
+            switcher = 1;
+            sessionStorage.setItem('menuSwitcher', 'collapsed');
+        }
+    }
 });
+
+function changeStyle(size, visibility) {
+    nav.style.width = size;
+    elements.forEach(element => {
+        element.style.visibility = visibility;
+    });
+}
+
